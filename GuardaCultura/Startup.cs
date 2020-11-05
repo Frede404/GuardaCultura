50b61f4549e7d2dd62106143b92f3ed8bac40f88
Frede404 <fredecabral@gmail.com>
2020-11-05 18:59:53 +0000

    alteracao de alguns textos e aspeto da pagina de apresentacao

--- GuardaCultura/Startup.cs
+++ GuardaCultura/Startup.cs
@@ -62,7 +62,7 @@ namespace GuardaCultura
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=privacy}/{id?}");//pagina inicial
                 endpoints.MapRazorPages();
             });
         }
