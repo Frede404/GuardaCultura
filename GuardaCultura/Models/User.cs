7a33482d4342f83b8c6459bf0b2003febc855257
Frede404 <fredecabral@gmail.com>
2020-11-26 10:19:37 +0000

    users

--- GuardaCultura/Models/User.cs
+++ GuardaCultura/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,30 @@ namespace GuardaCultura.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string Email { get; set; }
+
+        public string data_nasc { get; set; }
+
+        public string sexo { get; set; }
+
+        public string Nacionalidade { get; set; }
+
+        public float fiabilidade { get; set; }
+
+        public string lingua { get; set; }
+
+        public int privilegios { get; set; }
     }
 }
