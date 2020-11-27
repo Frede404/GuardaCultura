3dee6190ebc2c418ad6d80eeb1b110c47cf3376e
Leandro Gomes <leandrog276@gmail.com>
2020-11-27 23:13:24 +0000

    criacao das tabelas Hora, Ocupacao e Atratividade

---
+++ GuardaCultura/Models/Hora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCultura.Models
+{
+    public class Hora
+    {
+        public int HoraId { get; set; }
+        
+        [Required]
+        public string Horas { get; set; }
+        
+        public ICollection<Ocupacao> Ocupacaos { get; set; }
+        
+        public ICollection<Atratividade> Atratividades { get; set; }
+    }
+}
---
+++ GuardaCultura/Models/Ocupacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCultura.Models
+{
+    public class Ocupacao
+    {
+        public int OcupacaoId { get; set; }
+        
+        [Required]
+        public int Numero_pessoas { get; set; }
+        
+        [Required]
+        public string Data { get; set; }
+       
+        public int MiradouroId { get; set; }
+        public int HoraId { get; set; }
+
+        public ICollection<Miradouro> Miradouros { get; set; }
+
+    }
+}
