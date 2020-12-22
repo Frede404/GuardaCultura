3cf61ade7b96d955ad623f44077cbf9c6e2ffaba
Leandro Gomes <leandrog276@gmail.com>
2020-12-22 02:49:21 +0000

    alteracoes na pagina miradouros, e pagingInfo dos miradouros

---
+++ GuardaCultura/Models/MiradouroLista.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCultura.Models
+{
+    public class MiradouroLista
+    {
+         public IEnumerable<Miradouro> Miradouros { get; set; }
+
+        public PagingInfoMiradouro pagination { get; set; }
+
+        public int mira { get; set; }
+
+        public int ati { get; set; }
+    }
+}
